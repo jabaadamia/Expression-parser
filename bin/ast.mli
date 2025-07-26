a1e19ee8a29d6336bbda297372180092c2f13666
jaba <jabaadamia1@gmail.com>
2025-07-26 18:02:10 +0400

    defined tree structure

---
+++ bin/ast.mli
@@ -0,0 +1,26 @@
+
+type expr = 
+  | Arith of arithm_expr 
+  | Bool of bool_expr 
+and arithm_expr = 
+  | IConst of int 
+  | FConst of float 
+  | Neg of arithm_expr 
+  | Paren of arithm_expr 
+  | Add of arithm_expr * arithm_expr 
+  | Sub of arithm_expr * arithm_expr 
+  | Mul of arithm_expr * arithm_expr 
+  | Div of arithm_expr * arithm_expr 
+and bool_expr = 
+  | False 
+  | True 
+  | Not of bool_expr 
+  | BParen of bool_expr 
+  | Or of bool_expr * bool_expr 
+  | And of bool_expr * bool_expr 
+  | Eq of arithm_expr * arithm_expr 
+  | Neq of arithm_expr * arithm_expr 
+  | Geq of arithm_expr * arithm_expr 
+  | Leq of arithm_expr * arithm_expr 
+  | Le of arithm_expr * arithm_expr 
+  | Ge of arithm_expr * arithm_expr
